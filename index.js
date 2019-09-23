c3a94bf3bda2272558d79dcac67f39b2700969c5
Omar Mowafi <omar.mowafi@airbnb.com>
2019-09-23 16:34:00 +0200

    Add data to the countries list

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
-
+const countries_list = require('./countries_list');
+const countries_hash = countries_list.reduce((memo, elem) => {
+  memo[elem.country] = elem;
+  return memo;
+}, {})
 var mysql = require('mysql');
 
 const port = process.env.PORT || 3001;
@@ -54,7 +58,7 @@ app.get("/list", async (req, res) => {
   }
 });
 
-app.get("/:team_id", async (req, res) => {
+app.get("/team/:team_id", async (req, res) => {
   let { team_id } = req.params;
 
     try {
@@ -83,36 +87,22 @@ app.get("/:team_id", async (req, res) => {
 });
 
 app.get('/country.json', (req, res) => {
-  res.send({
-    "type": "FeatureCollection",
-    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-    "features": [
-      { "type": "Feature", "properties": { "meals": 87 }, "geometry": { "type": "Point", "coordinates": [ -151.5129, 63.1016, 0.0 ] } },
-      { "type": "Feature", "properties": { "meals": 46 }, "geometry": { "type": "Point", "coordinates": [ -150.4048, 63.1224, 105.5 ] } },
-      { "type": "Feature", "properties": { "meals": 129 }, "geometry": { "type": "Point", "coordinates": [ -151.3597, 63.0781, 0.0 ] } },
-      { "type": "Feature", "properties": { "meals": 42 }, "geometry": { "type": "Point", "coordinates": [ -118.497, 34.299667, 7.64 ] } },
-      { "type": "Feature", "properties": { "meals": 222 }, "geometry": { "type": "Point", "coordinates": [ -87.6901, 12.0623, 46.41 ] } },
-      { "type": "Feature", "properties": { "meals": 6 }, "geometry": { "type": "Point", "coordinates": [ -151.5053, 63.0719, 0.0 ] } },
-      { "type": "Feature", "properties": { "meals": 26 }, "geometry": { "type": "Point", "coordinates": [ -178.4576, -20.2873, 614.26 ] } },
-      { "type": "Feature", "properties": { "meals": 4 }, "geometry": { "type": "Point", "coordinates": [ -148.789, 63.1725, 7.5 ] } },
-      { "type": "Feature", "properties": { "meals": 39 }, "geometry": { "type": "Point", "coordinates": [ -120.993164, 36.421833, 6.37 ] } },
-      { "type": "Feature", "properties": { "meals": 876 }, "geometry": { "type": "Point", "coordinates": [ -117.0155, 33.656333, 12.37 ] } },
-      { "type": "Feature", "properties": { "meals": 11 }, "geometry": { "type": "Point", "coordinates": [ -151.512, 63.0879, 10.8 ] } },
-      { "type": "Feature", "properties": { "meals": 2 }, "geometry": { "type": "Point", "coordinates": [ -151.4378, 63.0933, 0.0 ] } },
-      { "type": "Feature", "properties": { "meals": 29 }, "geometry": { "type": "Point", "coordinates": [ -149.6538, 63.2272, 96.8 ] } },
-      { "type": "Feature", "properties": { "meals": 54 }, "geometry": { "type": "Point", "coordinates": [ -151.5325, 63.0844, 0.0 ] } },
-      { "type": "Feature", "properties": { "meals": 3 }, "geometry": { "type": "Point", "coordinates": [ -149.4752, 61.8518, 54.3 ] } },
-      { "type": "Feature", "properties": { "meals": 10 }, "geometry": { "type": "Point", "coordinates": [ -150.8597, 61.6214, 50.0 ] } },
-      { "type": "Feature", "properties": { "meals": 36 }, "geometry": { "type": "Point", "coordinates": [ -149.7142, 62.9656, 93.6 ] } },
-      { "type": "Feature", "properties": { "meals": 2 }, "geometry": { "type": "Point", "coordinates": [ -151.2484, 61.2705, 69.1 ] } },
-      { "type": "Feature", "properties": { "meals": 1980 }, "geometry": { "type": "Point", "coordinates": [ -152.0732, 65.5942, 14.8 ] } },
-      { "type": "Feature", "properties": { "meals": 1 }, "geometry": { "type": "Point", "coordinates": [ -90.5445, 13.5146, 54.36 ] } },
-      { "type": "Feature", "properties": { "meals": 38 }, "geometry": { "type": "Point", "coordinates": [ -118.819504, 37.605499, 4.14 ] } },
-      { "type": "Feature", "properties": { "meals": 98 }, "geometry": { "type": "Point", "coordinates": [ -118.930168, 37.636833, -0.71 ] } },
-      { "type": "Feature", "properties": { "meals": 658 }, "geometry": { "type": "Point", "coordinates": [ -117.509167, 34.1555, 16.34 ] } },
-    ]
-  }
-)})
+  con.query(
+    `select users_live.country, donations_live.team_id, sum(donations_live.meals) as meals_sum from donations_live, users_live where donations_live.user_id in (select distinct users_live.user_id) and donations_live.team_id = 'HendSabri' GROUP BY users_live.country, donations_live.team_id order by meals_sum desc`,
+    (err, rows) => {
+      if(err) throw err;
+      var ret = [];
+      rows.forEach(element => {
+        ret.push({ "type": "Feature", "properties": { "meals": element.meals_sum, country: element.country }, "geometry": { "type": "Point", "coordinates": [ countries_hash[element.country].longitude, countries_hash[element.country].latitude, 0.0 ] } },)
+      });
+      res.send({
+        "type": "FeatureCollection",
+        "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+        "features": ret
+      }
+    )
+  });
+})
 
 app.get('/team/:teamId.json', (req, res) => {
   res.send({
